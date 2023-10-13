18dbba80a0c6f69823c1f54d58bd822d1f71cdc6
vanessaadelineg <64440451+vanessaadelineg@users.noreply.github.com>
2023-10-13 10:45:04 +0200

    My commit

---
+++ .spyder-py3/temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+pip install tensorflow
+import tensorflow as tf
+!git add "file.py"
+!git commit -m "My commit"
+!git push origin master
